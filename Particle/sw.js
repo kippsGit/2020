66ba2ad56315e980b3b3d1409e338483f7f2f8d9
kippsGit <66156966+kippsGit@users.noreply.github.com>
2020-10-22 13:28:07 +0800

    Add files via upload

---
+++ Particle/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('Particle').then(function(cache) {
+     return cache.addAll([
+       'index.html',
+       'index.html?homescreen=1',
+       '?homescreen=1'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
