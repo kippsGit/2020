81fc1ab62dcdaeee03ebda651a96a9361c679335
kippsGit <66156966+kippsGit@users.noreply.github.com>
2020-10-24 16:28:29 +0800

    Add files via upload

--- Particle/sw.js
+++ Particle/sw.js
@@ -1,12 +1,56 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
+self.addEventListener('install', function(event) {
+  // Perform install step:  loading each required file into cache
+  event.waitUntil(
+    caches.open('Particle')
+      .then(function(cache) {
+        // Add all offline dependencies to the cache
+        return cache.addAll([
+          'index.html',
+          '/'
+          ]);
+      })
+      .then(function() {
+        // At this point everything has been cached
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return the response from the cached version
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - return the result from the live server
+        // `fetch` is essentially a "fallback"
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
+  event.waitUntil(self.clients.claim());
+});
+
+
+/*self.addEventListener('install', function(e) {
+ self.skipWaiting();
+e.waitUntil(
    caches.open('Particle').then(function(cache) {
      return cache.addAll([
-       'index.html'
+       'index.html',
+       '/'
      ]);
    })
- );
+);
 });
+
 
 self.addEventListener('fetch', function(event) {
  console.log(event.request.url);
@@ -16,7 +60,8 @@ self.addEventListener('fetch', function(event) {
      return response || fetch(event.request);
    })
  );
-});
+});*/
+
 
 /*self.addEventListener('fetch', function(event) {
   event.respondWith(
