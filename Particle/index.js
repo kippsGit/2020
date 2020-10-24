b06ad135d39b8739a047abaf683738b3bcc59748
kippsGit <66156966+kippsGit@users.noreply.github.com>
2020-10-24 17:21:52 +0800

    Add files via upload

---
+++ Particle/index.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	if('serviceWorker' in navigator) {
+	  navigator.serviceWorker
+	           .register('sw.js')
+	           .then(function() { console.log("Service Worker Registered"); });
+	}
+}
--- Particle/sw.js
+++ Particle/sw.js
@@ -1,4 +1,27 @@
-self.addEventListener('install', function(event) {
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('particle').then(function(cache) {
+     return cache.addAll([
+       'index.html',
+       'index.js'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
+
+/*self.addEventListener('install', function(event) {
   // Perform install step:  loading each required file into cache
   event.waitUntil(
     caches.open('Particle')
@@ -36,7 +59,7 @@ self.addEventListener('fetch', function(event) {
       }
     )
   );
-});
+});*/
 
 
 
