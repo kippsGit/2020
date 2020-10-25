93ea036dece836dfd449588f5e70bd6b4506191d
kippsGit <66156966+kippsGit@users.noreply.github.com>
2020-10-25 11:59:07 +0800

    Add files via upload

--- Flappy/sw.js
+++ Flappy/sw.js
@@ -4,6 +4,12 @@ self.addEventListener('install', function(e) {
      return cache.addAll([
        '/2020/Flappy/',
        '/2020/Flappy/index.html',
+       '/2020/Flappy/city.png',
+       '/2020/Flappy/flappy.ico',
+       '/2020/Flappy/flappy.png',
+       '/2020/Flappy/flappyBlue.png',
+       '/2020/Flappy/pipeBottom.png',
+       '/2020/Flappy/pipeTop.png'
      ]);
    })
  );
