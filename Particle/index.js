3cd24f7dde3cc9abe510372d8450ecfe29d5c7d0
kippsGit <66156966+kippsGit@users.noreply.github.com>
2020-11-30 15:00:49 +0800

    Add files via upload

--- Particle/index.js
+++ Particle/index.js
@@ -5,6 +5,7 @@ window.onload = function () {
 			.then(function (registration) { 
 				console.log("Service Worker Registered"); 
 				getHtmlElements("#updateAppController").onclick = function(){
+					document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
 					registration.unregister();
 					location.reload();
 				}
